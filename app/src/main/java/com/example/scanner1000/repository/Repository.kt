55687276a31cfaa46b518161d4f8ee5e42c2c7ff
SophiaB-code <sophia.bijak@gmail.com>
2024-02-25 23:15:22 +0100

    ReceiptScreen 30% TextRecognitionScreen 50% Database 50%

---
+++ app/src/main/java/com/example/scanner1000/repository/Repository.kt
@@ -0,0 +1,30 @@
+package com.example.scanner1000.repository
+
+import com.example.scanner1000.data.Category
+import com.example.scanner1000.data.CategoryDao
+import com.example.scanner1000.data.Product
+import com.example.scanner1000.data.ProductDao
+
+class Repository(
+    private val productDao: ProductDao,
+    private val categoryDao: CategoryDao
+) {
+    fun getProductWithCategory(id: Int) = categoryDao
+        .getProductsWithCategory()
+    suspend fun insertCategory(category: Category) {
+        categoryDao.upsertCategory(category)
+    }
+
+    suspend fun insertProduct(product: Product) {
+        productDao.upsertProduct(product)
+    }
+
+    suspend fun deleteItem(product: Product) {
+        productDao.deleteProduct(product)
+    }
+
+//    suspend fun updateItem(item: Item) {
+//        itemDao.update(item)
+//    }
+
+}
